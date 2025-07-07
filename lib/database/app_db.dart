1bf982a97fa75874d3617b30e5e964b5d1473808
Melkizedeki <mmagota2@gmail.com>
2025-07-07 17:03:33 +0300

    The commit after first configurations of the database

---
+++ lib/database/app_db.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart';
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(path.join(dbFolder.path, 'favourite_place.sqlite'));
+    return NativeDatabase(file);
+  });
+}
